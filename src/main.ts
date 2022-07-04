4e217373aab82d9f5a8f694e090a6578068f79aa
Junior Vasquez <jvasquez.x1@gmail.com>
2022-07-04 12:00:11 -0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import App from './App.svelte';
 import './style/tailwind.css';
 
 const app = new App({
-  target: document.getElementById('app')
+  target: document.getElementById('app') || document.body
 });
 
 export default app;
